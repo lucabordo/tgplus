3f27d6e3c12d5d5973243db716ba8da50c8e4f02
Lucas Bordeaux <lucabordo@hotmail.co.uk>
2023-06-07 10:47:27 +0100

    Basic curated training and test sets.

--- tgplus/data.py
+++ tgplus/data.py
@@ -2,9 +2,12 @@
 Access and basic processing of movies data.
 """
 import json
-from typing import List
+from typing import List, Tuple
+
 import pandas as pd
-from tgplus.globals import GENRES_TAXONOMY, DATA_CACHE
+from numpy.random import RandomState
+
+from tgplus.globals import GENRES_TAXONOMY, DATA_CACHE, TextWithGenres
 
 
 def get_movies_table() -> pd.DataFrame:
@@ -50,3 +53,33 @@ def parse_genres(genre_entry: str) -> List[str]:
     assert all(isinstance(genre, dict) for genre in result)
     assert all(set(genre.keys()) == {"id", "name"} for genre in result)
     return [genre["name"] for genre in result]
+
+
+def get_movies_data(test_fraction=0.1) -> Tuple[TextWithGenres, TextWithGenres]:
+    """
+    Load a dataset that has a bit of pre-processing and some splitting.
+    """
+    # Load as data frame:
+    movies_table = get_movies_table()
+
+    # There is some non-English content and we focus on English, for now:
+    english_subset = movies_table[movies_table["original_language"] == "en"]
+
+    # We gather text (we use overview but also - debatable - title) and genres for each entry:
+    data = []
+    subcolumns = zip(english_subset["title"], english_subset["overview"], english_subset["genres"])
+
+    for title, overview, genres in subcolumns:
+        text = f"{title}. {overview}"
+        genre_names = parse_genres(genres)
+        data.append((text, genre_names))
+    
+    # Split deterministically into two collections - we shamefully do not use validation here:
+    prng = RandomState(1245737)
+    test_size = int(len(data) * test_fraction)
+    test_indices = set(prng.choice(len(data), test_size, replace=False))
+    
+    test_subset = [entry for index, entry in enumerate(data) if index in test_indices]
+    train_subset = [entry for index, entry in enumerate(data) if index not in test_indices]
+
+    return train_subset, test_subset
--- tgplus/globals.py
+++ tgplus/globals.py
@@ -2,6 +2,7 @@
 Global definitions common to the data-loading, training code, app... 
 """
 from pathlib import Path
+from typing import List, Tuple, TypeAlias
 import tgplus
 
 
@@ -10,6 +11,13 @@ PROJECT_ROOT = Path(tgplus.__path__[0]).parent
 
 # Path where we put any data loaded from the internet:
 DATA_CACHE = PROJECT_ROOT / "data"
+
+# Collection of text data annotated by genre; Each entry pairs: 
+# - textual description of the movie;
+# - list of genres associated to the movie.
+# This is a common representation for data from any source, 
+# and whether the collection is used for training, validation or test:
+TextWithGenres: TypeAlias = List[Tuple[str, List[str]]]
 
 
 # Immutable list of genres we use;
